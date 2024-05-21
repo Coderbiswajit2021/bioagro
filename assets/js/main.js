16a1cc5904c6ed49b4f9db976f377a295ba35c3b
Coderbiswajit2021 <sahoobiswajit969@gmail.com>
2024-05-21 11:02:12 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+   const toggle = document.getElementById(toggleId),
+         nav = document.getElementById(navId)
+
+   toggle.addEventListener('click', () =>{
+       // Add show-menu class to nav menu
+       nav.classList.toggle('show-menu')
+
+       // Add show-icon to show and hide the menu icon
+       toggle.classList.toggle('show-icon')
+   })
+}
+
+showMenu('nav-toggle','nav-menu')
+
+
+// review section 
+
+var swiper = new Swiper(".ReviewSlide", {
+    slidesPerView: 1,
+    grabCuror: true,
+    loop: true,
+    spaceBetween: 10,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        700: {
+            slidesPerView: 2,
+        },
+        1050: {
+            slidesPerView: 3,
+        },
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+
+    },
+});
